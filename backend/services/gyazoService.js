5af3a735e188c8b04ca5b474a027181c3668a100
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-06-29 11:43:51 +0200

    Change gyazo utl to env

--- backend/services/gyazoService.js
+++ backend/services/gyazoService.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 import fs from "fs";
 import FormData from "form-data";
+import dontenv from "dotenv";
+
+dontenv.config();
 
 export async function uploadToGyazo(localFilePath) {
   const form = new FormData();
   form.append("imagedata", fs.createReadStream(localFilePath));
   form.append("access_token", process.env.GYAZO_ACCESS_TOKEN);
 
-  const response = await axios.post(
-    "https://upload.gyazo.com/api/upload",
-    form,
-    {
-      headers: form.getHeaders(),
-    }
-  );
+  const response = await axios.post(process.env.GYAZO_ENDPOINT, form, {
+    headers: form.getHeaders(),
+  });
 
   return response.data;
 }
